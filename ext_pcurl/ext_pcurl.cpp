b9d059f48d1766b62818571f5e34757415fe2245
Alexander Duer <mail.duer@gmail.com>
2015-10-14 11:47:15 +0200

    removed duplicate loadSystemLib() call

--- ext_pcurl/ext_pcurl.cpp
+++ ext_pcurl/ext_pcurl.cpp
@@ -2799,8 +2799,8 @@ static std::string s_namedPools;
 class PCurlExtension final : public Extension {
   /* CUSTOM_START */
   private:
-    int threadCount = 10;
-    int cleanupIntervalSec = 60;
+    int threadCount;
+    int cleanupIntervalSec;
   /* CUSTOM_END */
 
  public:
@@ -2808,12 +2808,12 @@ class PCurlExtension final : public Extension {
 
   /* CUSTOM_START */
   virtual void moduleLoad(const IniSetting::Map& ini, Hdf hdf) {
-    //Hdf hdf_pcurl = hdf["PCurl"];
-    //Hdf hdf_server = hdf["Server"];
+    Hdf hdf_pcurl = hdf["PCurl"];
+    Hdf hdf_server = hdf["Server"];
 
-    //threadCount = Config::GetInt32(ini, hdf, "ThreadCount", 10, false);
-    //cleanupIntervalSec =
-    //  Config::GetInt32(ini, hdf, "CleanupIntervalSec", 60, false);
+    threadCount = Config::GetInt32(ini, hdf, "ThreadCount", 10, false);
+    cleanupIntervalSec =
+      Config::GetInt32(ini, hdf, "CleanupIntervalSec", 60, false);
 
     //_LOG("extension pcurl: created");
   }
@@ -2825,7 +2825,7 @@ class PCurlExtension final : public Extension {
       std::make_shared<HostSocketFdPool>(threadCount, cleanupIntervalSec);
     //registerConstants();
     //registerFunctions();
-    loadSystemlib();
+    //loadSystemlib();
     //_LOG("extension pcurl: initialized");
     /* CUSTOM_END */
 
